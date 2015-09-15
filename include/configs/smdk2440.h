8ac8ae8585810119d55468c28ca235adbbcd5a90
Xuegui.Bao <brain0822@126.com>
2015-09-16 03:10:39 +0800

    报错

--- include/configs/smdk2440.h
+++ include/configs/smdk2440.h
@@ -98,7 +98,7 @@
 #define CONFIG_CMD_DATE
 #define CONFIG_CMD_DHCP
 #define CONFIG_CMD_ELF
-#define CONFIG_CMD_NAND
+//#define CONFIG_CMD_NAND
 #define CONFIG_CMD_PING
 #define CONFIG_CMD_REGINFO
 #define CONFIG_CMD_USB
@@ -223,7 +223,7 @@
 #define CONFIG_CMD_MTDPARTS
 #define CONFIG_MTD_DEVICE
 #define CONFIG_MTD_PARTITIONS
-#define CONFIG_YAFFS2
+//#define CONFIG_YAFFS2
 #define CONFIG_RBTREE
 
 /* additions for new relocation code, must be added to all boards */
